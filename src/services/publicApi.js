e7b26792884a612083a017ea736c19017389b0cb
DeebaAnnular <deebalakshmi.m@annulartechnologies>
2024-07-26 19:59:10 +0530

    public api changed to de url from local url

--- src/services/publicApi.js
+++ src/services/publicApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const publicAPI = axios.create({
-    baseURL:"http://localhost:3000"
+    baseURL:"http://13.201.79.49:9091"
+    // "http://localhost:3000"
 });
 
 export default publicAPI;
