2a889ad8e81ee7bbe46b2a978f773895383f0c8a
sreenathAnnular <sreenath.p@annulartechnologies.com>
2024-08-08 15:39:30 +0530

    added SSL certificate

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,35 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//      output:"standalone"
+// };
+
+// export default nextConfig; 
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//      output:"standalone"
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     output:"standalone"
-};
-
-export default nextConfig;
+     output: "standalone",
+     serverRuntimeConfig: {
+       // Private key for SSL/TLS
+   
+       sslKey: "/etc/letsencrypt/live/lms-api.annularprojects.com/privkey.pem",
+       // SSL/TLS certificate
+       sslCert: "/etc/letsencrypt/live/lms-api.annularprojects.com/fullchain.pem",
+     },
+     async rewrites() {
+       return [
+         {
+           source: "/",
+           destination: "lms-api.annularprojects.com:3000",
+         },
+       ];
+     },
+   };
+   
+   export default nextConfig;
